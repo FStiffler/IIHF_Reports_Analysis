e13b1fd886d4885dd259d5728270a529d5540581
FStiffler <fstiffler@gmx.ch>
2021-10-20 20:57:14 +0200

    Adpated code that it can read every pdf file even when survey structure changes. Added a new function for this purpose which detects and handles data in landscape page format.

--- ExtractReportData.R
+++ ExtractReportData.R
@@ -56,24 +56,34 @@ gc()
 # Extract data from reports ####
 
 # Extract entire text from pdf
-text<-pdf_text("Reports/iihf2011.pdf")
+text<-pdf_text("Reports/iihf2021.pdf")
 
-# Extract player survey page as vector
-survey_page<-text[str_detect(text, "IIHF Survey of Players")][2] 
+# Extract player survey pages as vector
+start_page<-which(str_detect(text, "IIHF Survey of Players|IIHF SURVEY OF PLAYERS"))[2]  # page number of survey start
+end_page<-which(str_detect(text, "CIA World Factbook"))  # page number of survey end
+survey_page<-paste(text[start_page:end_page], collapse = "")  # concatenate survey pages to one vector
 
 # Create and clean list with each page line as separate entry
-page_list<-as.list(str_split(survey_page, "\n")[[1]])%>%  # Create list
+page_list<-as.list(str_split(survey_page, "\n")[[1]])%>%  # Create list with lines of page as entries
   str_squish%>%  # Remove excessice empty spaces between character
+  lapply(handle_landscaped)%>%  # Check if data in landscape format
+  unlist()%>% 
   str_remove_all("(?<=\\,)\\s")%>%  # Remove empty space between participated WM's
   str_remove_all("\\s(?=I|V)")%>%  # Remove empty spaces in front of roman numbers 
+  str_replace_all("Championships/Olympic Games", "Championships")%>%  # In olympic game year, remove olympic games from name 
   str_remove_all("(?<=Outdoor ).+(?=Championships)")%>%  # Only one empty space between Outdoor and Championships
   str_replace_all("Male FemaleIndoor", "MaleRefs FemaleRefs Indoor")%>%  # Rename variables for extraction later
-  str_replace_all("(?<=\\s\\d{1,5})\\s(?=\\d{1,3}\\,)", " None ")%>%  # Insert NA if not WM participation
-  lapply(correct_names)
+  str_remove_all("(?<=\\d)\\,(?=\\d)")%>%  # Remove all commas between numbers (normally thousander separators)
+  str_remove_all("(?<=\\d)\\.(?=\\d)")%>%  # Remove all points between numbers (normally thousander separators)
+  str_replace_all("(?<=\\s\\d{1,6})\\s(?=\\d{1,10}$)", " None ")%>%  # Insert None if a country has played in no championship
+  lapply(correct_names)%>%
+  #Remove report specific errors
+  str_remove_all("(?<=WM|WW)\\s(?=\\d{2}IQ)")%>%  # Error in championship list for austria (iihf2014)
+  str_replace_all("WM18 WWIA", "WM18,WWIA")  # Error in championship list for slovakia (iihf2018)
   
 # Subset List
-header<-page_list[str_detect(page_list,"Registered")] # Header of table
-country_list<-page_list[str_detect(page_list, "\\d+\\s\\d+")] # Actual data from table 
+header<-page_list[str_detect(page_list,"Registered")][1] # Header of table (if two pages select first header)
+country_list<-page_list[str_detect(page_list, "\\d+\\s\\d+\\s\\d+")] # Actual data from table 
 
 # Prepare header to be inserted into tibble
 header<-c("Country", as.vector(str_split(header, " ", simplify = T)))
@@ -86,18 +96,20 @@ data_tibble<-tibble(x=unlist(country_list))%>%
 # Prepare tibble for analysis
 final_data<-data_tibble%>%
   mutate(Country=str_replace_all(Country, "_", " "))%>%  # Replace underscore in country names again
-  mutate(Population=str_remove_all(Population, ","))%>%  # Remove comma in population number
   mutate(Championships=str_remove_all(Championships, "\\*"))%>%  # Remove asteriks in string
+  mutate(Championships=str_remove_all(Championships, "A|B|Q"))%>%  # Remove information about division groups or qualification
   mutate_at(vars(header[c(-1,-10)]), as.numeric)%>%  # Transform to numeric variables
   mutate(Championships=str_split(Championships, ","))%>%  # split Championships column in list if sublists
   unnest(Championships)%>%  # Unnest list to create entry for each championship participation
   add_column(values=rep(1))%>%  # Create artifical dummy for pivot longer afterwards
+  distinct()%>%  # Remove potential duplicates
   pivot_wider(names_from = Championships, values_from = values, values_fill=0)%>%  # Create dummy columns for each championship
   select(-None)%>%  # Remove None dummy column
   rename(IndoorRinks=Indoor,
          OutdoorRinks=Outdoor)
 
 # Reorder variables based on championships
+length(colnames(final_data))==length(sort_variables(colnames(final_data)))
 colnames(final_data)<-sort_variables(colnames(final_data)) 
   
   
--- Ressources/helperFunctions.R
+++ Ressources/helperFunctions.R
@@ -1,5 +1,8 @@
-# Helper function to correct country names in the page list for further processing
 correct_names<-function(x){
+  
+  #' @description Helper function to correct country names in the page list for further processing
+  #' @param x character. Vector with country information
+  #' @returns Line with adjusted names wherever necessary 
   
   # detect pattern "namecomponent1 namecomponent2" in front of number
   if(str_detect(x, "^[A-Za-z]+\\s[A-Za-z]+(?=\\s\\d)")){
@@ -9,6 +12,7 @@ correct_names<-function(x){
     country<-str_replace_all(country, "\\s", "\\_")  # replace white space by underscore
     line<-paste0(country, remaining)  # concatenate the two strings to create new string
     return(line)  # return new string
+    
   }
   
   # detect pattern "namecomponent1 namecomponent2 namecomponent3" in front of number
@@ -29,14 +33,32 @@ correct_names<-function(x){
     country<-str_replace_all(country, "\\s", "\\_")
     line<-paste0(country, remaining)
     return(line)
+    
   }
   
-  else return(x)
+  
+  # detect pattern "namecomponent1 namecomponent3,namecomponent4" in front of number
+  else if(str_detect(x, "^[A-Za-z]+\\s[A-Za-z]+\\,[A-Za-z]+(?=\\s\\d)")){
+    
+    country<-str_extract(x, "^[A-Za-z]+\\s[A-Za-z]+(?=\\,)")
+    remaining<-str_split(x, "^[A-Za-z]+\\s[A-Za-z]+\\,[A-Za-z]+(?=\\s\\d)")[[1]][2]
+    country<-str_replace_all(country, "\\s", "\\_")
+    line<-paste0(country, remaining)
+    return(line)
+    
+  }
+  
+  else{
+    return(x)
+  }
   
 }
 
-# Sort variables in a meaningfull way based on championships 
 sort_variables<-function(x){
+  
+  #' @description Sorts variables in a meaningfull way based on championship names.
+  #' @param x character. Vector containing all column names
+  #' @returns Vector with ordered column names based on championship names. 
   
   new<-c(
     x[1:10],
@@ -44,10 +66,55 @@ sort_variables<-function(x){
     sort(x[!is.na(str_match(x, "WM(I{1,3}|IV|V|VI)$"))]),  # men championships lower divisions
     sort(x[!is.na(str_match(x, "WM20"))]),  # U20 men championships
     sort(x[!is.na(str_match(x, "WM18"))]),  # U18 men championships
+    sort(x[!is.na(str_match(x, "OGM"))]),  # men olympic games
     sort(x[!is.na(str_match(x, "WW$"))]),  # women championship top division
     sort(x[!is.na(str_match(x, "WW(I{1,3}|IV|V|VI)$"))]),  # men championships lower divisions
-    sort(x[!is.na(str_match(x, "WW18"))])  # U18 men championships
+    sort(x[!is.na(str_match(x, "WW18"))]),  # U18 men championships
+    sort(x[!is.na(str_match(x, "OGW$"))])  # Women olympic games
   )
   
   return(new)
 }
+
+handle_landscaped<-function(x){
+  
+  #' @description Seperates country information when the player survey page is in landscape format. In this case, information of two countries might be contained in the same page line
+  #' @param x character. A string containing the information of one page line
+  #' @returns List containing the separated country information for each page line if necessary.
+    
+  # Header is duplicated. Separate and only return one instance of header
+  if(str_detect(x, "Registered")){
+      
+    return(str_split(x, "\\s(?=Registered)")[[1]][1])
+      
+  }
+    
+  # Separate strings which contain information about two countries
+  else if(str_detect(x, "\\d{2}(\\,|\\.)\\d{3}\\s[A-Za-z]")){
+    
+    first<-str_extract(x, "^.+?\\d{2}(\\,|\\.)\\d{3}(?=\\s[A-Za-z])")  # information of first country
+    second<-str_trim(str_split(x, "^.+?\\d{2}(\\,|\\.)\\d{3}(?=\\s[A-Za-z])")[[1]][2])  # information of second country
+    return(list(first,second))
+      
+  } 
+    
+  # Remove summarized numbers from country information on same line (Denmark in iihr2021 for example). 
+  # Explanation of approach:
+  # The summarized numbers are of at list 4 digits. 
+  # No country has 4 digit numbers in population,  indoor and outdoor rinks without also having participated 
+  # in at least one championship. Therefore, search for 3 consecutive 4 or more digit numbers at end of string.
+  else if(str_detect(x, "(\\d{1,3}(\\.|\\.)\\d{3})\\s(\\d{1,3}(\\.|\\.)\\d{3})\\s(\\d{1,3}(\\.|\\.)\\d{3})$")){  
+      
+    return(str_extract(x, "^.+?\\d{2}(\\,|\\.)\\d{3}"))  # Extract information about country
+      
+      
+  }
+    
+  else {
+    return(x)
+  }
+      
+
+    
+}
+    
