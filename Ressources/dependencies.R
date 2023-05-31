d55d44d3ebd10876879fafccd1d7446fc145cfac
FStiffler <fstiffler@gmx.ch>
2023-05-31 22:32:17 +0200

    Add new data and dependencies for tournament analysis

--- Ressources/dependencies.R
+++ Ressources/dependencies.R
@@ -1,5 +1,5 @@
 ##### Set dependencies
-dependecies <- c("tidyverse", "rvest", "pdftools", "lubridate", "RColorBrewer")
+dependecies <- c("tidyverse", "rvest", "pdftools", "lubridate", "RColorBrewer", "knitr","kableExtra")
 
 ##### Install and load packages
 missing <- dependecies[!(dependecies %in% installed.packages()[,"Package"])]
